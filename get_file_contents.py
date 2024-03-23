14616617c8a42da7e45e0c61d47f2bfa329d494e
FlynntKnapp <bruce.stull@gmail.com>
2024-03-23 05:07:25 -0400

    Converted 'get_file_content' to a semi-pure python function

--- get_file_contents.py
+++ get_file_contents.py
@@ -1,10 +1,20 @@
 # get_file_contents.py
 
-from base import get_file_content
+import os
+
+from dotenv import load_dotenv
+
+from base import get_github_file_content
+
+load_dotenv()
+
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
+REPO = os.getenv("REPO")
 
 file_to_get = "unimportant_notes/models.py"
 
-file_content = get_file_content(file_to_get)
+file_content = get_github_file_content(USERNAME, TOKEN, REPO, file_to_get)
 
 if file_content:
     print(f"\nContents of '{file_to_get}':\n", file_content)
